71324a85e186bc41c26c0a7c8fa70df8edbada2a
Mathias Sandvold <mathias.sandvold@nordicbatteries.com>
2023-09-12 15:48:34 +0200

    idk

--- functions/functions.h
+++ functions/functions.h
@@ -20,9 +20,61 @@
 
 /*========== Extern Constant and Variable Declarations ======================*/
 
-extern double num1;
-extern double num2;
-extern char op;
-extern double ans;
+/**
+ * @brief   Take input from user for first number.
+ * @param   num1 pointer to first number.
+ */
+void inputNum1(double *num1) {
+    printf("Enter first number: ");
+    scanf("%lf", num1);
+}
+
+/**
+ * @brief   Take input from user for operator.
+ * @param   op pointer to operator.
+ */
+void inputOp(char *op) {
+    printf("Enter operator: ");
+    scanf(" %c", op);
+}
+
+/**
+ * @brief   Take input from user for second number.
+ * @param   num2 pointer to first number.
+ */
+void inputNum2(double *num2) {
+    printf("Enter second number: ");
+    scanf("%lf", num2);
+}
+
+/* 
+*   Function to perform the calculation based on operator
+ */
+/**
+ * @brief   Calculate answer.
+ * @param   num1 pointer to first number.
+ * @param   num2 pointer to second number.
+ * @param   op pointer to first number.
+ * @return  answer.
+ */
+double calc(double num1, double num2, char op) {
+    double ans = 0;
+    if (op == '+') {
+        ans = num1 + num2;
+    } else if (op == '-') {
+        ans = num1 - num2;
+    } else if (op == '*') {
+        ans = num1 * num2;
+    } else if (op == '/') {
+        if (num2 != 0) {
+            ans = num1 / num2;
+        } else {
+            printf("Division by zero is not allowed.\n");
+        }
+    } else {
+        printf("Invalid operator.\n");
+    }
+    return ans;
+}
 
 /*========== Extern Function Prototypes =====================================*/
